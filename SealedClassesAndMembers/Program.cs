36ef990548e8d98800e2cf00bb4aeaae2d3176d2
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-08-18 05:22:33 +0200

    Add project files.

---
+++ SealedClassesAndMembers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Sealed Modifier
+    - prevents derivation of classes or overriding of methods
+    - sealed classes are slightly faster because of some run-time optimizations
+ 
+ */
+
+namespace SealedClassesAndMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
